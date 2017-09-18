c9b61a0c0a262bd4f9ff05b22816bae7fd96dd43
Dominic Trang <dominic_t@hotmail.co.nz>
2017-09-18 14:44:10 +1200

    asd

--- backstop.js
+++ backstop.js
@@ -41,7 +41,7 @@ module.exports = {
     "html_report": "backstop_data/html_report",
     "ci_report": "backstop_data/ci_report"
   },
-  "report": ["browser"],
+  "report": [],
   "engine": "phantomjs",
   "casperFlags": [],
   "asyncCaptureLimit": 5,
